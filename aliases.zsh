1a4d19e493152c22ffeda7c585668e48efeb85a7
James Lambie <jameslambie@gmail.com>
2021-07-29 09:25:33 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,8 @@ alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
+alias pw='pwd | pbcopy'
+alias ducks='du -cks -- * | sort -rn | head'
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -32,3 +34,19 @@ alias resolve="git add . && git commit --no-edit"
 alias stash="git stash -u"
 alias unstage="git restore --staged ."
 alias wip="commit wip"
+
+# Projects
+
+# AAF Project Shortcuts
+alias aaf-api='cd $HOME/projects/27works/appliedartforms/api && npm start'
+alias aaf-cdn='cd $HOME/projects/27works/appliedartforms/cdn && npm start'
+alias aaf-cms='cd $HOME/projects/27works/appliedartforms/admin && npm start'
+alias aaf-web='cd $HOME/projects/27works/appliedartforms/web && npm run dev'
+alias aaf='cd $HOME/projects/27works/appliedartforms && code . && aaf-api'
+
+# SKH Project Shortcuts
+alias skh-api='cd $HOME/projects/27works/simonelli-hub/api && npm start'
+alias skh-cdn='cd $HOME/projects/27works/simonelli-hub/cdn && npm start'
+alias skh-cms='cd $HOME/projects/27works/simonelli-hub/admin && npm start'
+alias skh-web='cd $HOME/projects/27works/simonelli-hub/web && npm run dev'
+alias skh='cd $HOME/projects/27works/simonelli-hub && code . && skh-api'
